2fa07867ad3b086a24b9e9a37b58b526183c6197
Alex Jercan <jercan_alex27@yahoo.com>
2024-02-21 20:41:27 +0200

    refactor: move code into c folder

--- checker.sh
+++ checker.sh
@@ -1,36 +1,14 @@
 #!/bin/bash
 
-lexical_analyzer() {
-    echo "Testing the lexical analyzer"
-
-    passed=0
-    for file_path in $(ls tests/lexer/*.cl); do
-        ref_path=./tests/lexer/$(basename $file_path .cl).ref
-
-        file_name=$(basename $file_path)
-        echo -en "Testing $file_name ... "
-
-        make run -si ARGS="--lex $file_path" | diff - $ref_path > /dev/null 2>&1
-
-        if [ $? -eq 0 ]; then
-            echo -e "\e[32mPASSED\e[0m"
-            passed=$((passed + 1))
-        else
-            echo -e "\e[31mFAILED\e[0m"
-        fi
-    done
-
-    total=$(ls ./tests/lexer/*.cl | wc -l)
-    echo "Passed $passed/$total tests"
-}
-
-make clean && make
-
-ARG1=$1
-if [ "$ARG1" == "--lex" ]; then
-    lexical_analyzer
-elif [ -z "$ARG1" ]; then
-    lexical_analyzer
-else
-    echo "Invalid argument"
+PROJECT=$1
+if [ -z "$PROJECT" ]; then
+    echo "Usage: $0 <project> [args]"
+    exit 1
 fi
+
+ARGS=${@:2}
+
+docker build -t "cool-compiler-$PROJECT" ./$PROJECT \
+    && docker run -v $(pwd)/checker/tests:/app/tests \
+                  -v $(pwd)/checker/checker.sh:/app/checker.sh \
+                  "cool-compiler-$PROJECT" sh /app/checker.sh $ARGS
