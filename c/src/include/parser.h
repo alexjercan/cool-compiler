3b67680abbb29cce2d270f955a32d7d1720c999f
Alex Jercan <jercan_alex27@yahoo.com>
2024-02-25 15:56:34 +0200

    feat: add more stuff

--- c/src/include/parser.h
+++ c/src/include/parser.h
@@ -16,18 +16,104 @@ typedef struct node_info {
 
 enum expr_type {
     EXPR_NONE,
+    EXPR_ASSIGN,
+    EXPR_STATIC_DISPATCH,
+    EXPR_DISPATCH,
+    EXPR_COND,
+    EXPR_LOOP,
+    EXPR_BLOCK,
+    EXPR_LET,
+    EXPR_CASE,
+    EXPR_NEW,
+    EXPR_ISVOID,
+    EXPR_ADD,
+    EXPR_SUB,
+    EXPR_MUL,
+    EXPR_DIV,
+    EXPR_NEG,
+    EXPR_LT,
+    EXPR_LE,
+    EXPR_EQ,
+    EXPR_NOT,
+    EXPR_PAREN,
+    EXPR_IDENT,
     EXPR_INT,
     EXPR_STRING,
     EXPR_BOOL,
-    EXPR_IDENT,
 };
+
+typedef struct expr_unary_node {
+        struct expr_node *expr;
+} expr_unary_node;
+
+typedef struct expr_binary_node {
+        struct expr_node *lhs;
+        struct expr_node *rhs;
+} expr_binary_node;
+
+typedef struct let_init_node {
+        node_info name;
+        node_info type;
+        struct expr_node *init;
+} let_init_node;
+
+typedef struct case_node {
+        node_info name;
+        node_info type;
+        struct expr_node *body;
+} branch_node;
 
 typedef struct expr_node {
         enum expr_type type;
         union {
+                struct {
+                        node_info name;
+                        struct expr_node *value;
+                } assign;
+                struct {
+                        struct expr_node *expr;
+                        node_info type;
+                        node_info method;
+                        ds_dynamic_array args; // expr_node
+                } static_dispatch;
+                struct {
+                        struct expr_node *expr;
+                        node_info method;
+                        ds_dynamic_array args; // expr_node
+                } dispatch;
+                struct {
+                        struct expr_node *predicate;
+                        struct expr_node *then;
+                        struct expr_node *else_;
+                } cond;
+                struct {
+                        struct expr_node *predicate;
+                        struct expr_node *body;
+                } loop;
+                ds_dynamic_array block;
+                struct {
+                        ds_dynamic_array inits; // let_init_node
+                        struct expr_node *body;
+                } let;
+                struct {
+                        struct expr_node *expr;
+                        ds_dynamic_array cases; // branch_node
+                } case_;
+                node_info new;
+                struct expr_node *isvoid;
+                expr_binary_node add;
+                expr_binary_node sub;
+                expr_binary_node mul;
+                expr_binary_node div;
+                expr_unary_node neg;
+                expr_binary_node lt;
+                expr_binary_node le;
+                expr_binary_node eq;
+                expr_unary_node not ;
+                struct expr_node *paren;
                 node_info ident;
-                node_info string;
                 node_info integer;
+                node_info string;
                 node_info boolean;
         };
 } expr_node;
--- c/src/parser.c
+++ c/src/parser.c
@@ -3,35 +3,6 @@
 #include "lexer.h"
 #include <stdarg.h>
 
-static void node_info_init(node_info *info) { info->value = NULL; }
-
-static void expr_node_init(expr_node *expr) { expr->type = EXPR_NONE; }
-
-static void attribute_node_init(attribute_node *attribute) {
-    node_info_init(&attribute->name);
-    node_info_init(&attribute->type);
-    expr_node_init(&attribute->value);
-}
-
-static void formal_node_init(formal_node *formal) {
-    node_info_init(&formal->name);
-    node_info_init(&formal->type);
-}
-
-static void method_node_init(method_node *method) {
-    node_info_init(&method->name);
-    node_info_init(&method->type);
-    ds_dynamic_array_init(&method->formals, sizeof(formal_node));
-    expr_node_init(&method->body);
-}
-
-static void class_node_init(class_node *class) {
-    node_info_init(&class->name);
-    node_info_init(&class->superclass);
-    ds_dynamic_array_init(&class->attributes, sizeof(attribute_node));
-    ds_dynamic_array_init(&class->methods, sizeof(method_node));
-}
-
 struct parser {
         const char *filename;
         ds_dynamic_array *tokens;
@@ -115,11 +86,498 @@ static void parser_show_errorf(struct parser *parser, const char *format, ...) {
         parser, "expected %s or %s, got %s", token_type_to_string(expected1),  \
         token_type_to_string(expected2), token_type_to_string(got))
 
+static void build_expr(struct parser *parser, expr_node *expr);
+
+static void build_expr_if(struct parser *parser, expr_node *expr) {
+    struct token token;
+
+    expr->type = EXPR_COND;
+    expr->cond.predicate = malloc(sizeof(expr_node));
+    expr->cond.then = malloc(sizeof(expr_node));
+    expr->cond.else_ = malloc(sizeof(expr_node));
+
+    parser_current(parser, &token);
+    if (token.type != IF) {
+        parser_show_expected(parser, IF, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    build_expr(parser, expr->cond.predicate);
+
+    parser_current(parser, &token);
+    if (token.type != THEN) {
+        parser_show_expected(parser, THEN, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    build_expr(parser, expr->cond.then);
+
+    parser_current(parser, &token);
+    if (token.type != ELSE) {
+        parser_show_expected(parser, ELSE, token.type);
+        if (token.type == END || token.type == FI) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    build_expr(parser, expr->cond.else_);
+
+    parser_current(parser, &token);
+    if (token.type != FI) {
+        parser_show_expected(parser, FI, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+}
+
+static void build_expr_while(struct parser *parser, expr_node *expr) {
+    struct token token;
+
+    expr->type = EXPR_LOOP;
+    expr->loop.predicate = malloc(sizeof(expr_node));
+    expr->loop.body = malloc(sizeof(expr_node));
+
+    parser_current(parser, &token);
+    if (token.type != WHILE) {
+        parser_show_expected(parser, WHILE, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    build_expr(parser, expr->loop.predicate);
+
+    parser_current(parser, &token);
+    if (token.type != LOOP) {
+        parser_show_expected(parser, LOOP, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    build_expr(parser, expr->loop.body);
+
+    parser_current(parser, &token);
+    if (token.type != POOL) {
+        parser_show_expected(parser, POOL, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+}
+
+static void build_expr_block(struct parser *parser, expr_node *expr) {
+    struct token token;
+
+    expr->type = EXPR_BLOCK;
+    ds_dynamic_array_init(&expr->block, sizeof(expr_node));
+
+    parser_current(parser, &token);
+    if (token.type != LBRACE) {
+        parser_show_expected(parser, LBRACE, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    parser_current(parser, &token);
+    while (token.type != RBRACE) {
+        if (token.type == END) {
+            parser_show_expected(parser, RBRACE, token.type);
+            return;
+        }
+
+        expr_node expr;
+
+        build_expr(parser, &expr);
+
+        ds_dynamic_array_append(&expr.block, &expr);
+
+        parser_current(parser, &token);
+        if (token.type != SEMICOLON) {
+            parser_show_expected(parser, SEMICOLON, token.type);
+            if (token.type == END || token.type == RBRACE) {
+                return;
+            }
+        }
+    }
+    parser_advance(parser);
+}
+
+static void build_let_init(struct parser *parser, let_init_node *init) {
+    struct token token;
+
+    init->name.value = NULL;
+    init->type.value = NULL;
+    init->init = malloc(sizeof(expr_node));
+
+    parser_current(parser, &token);
+    if (token.type == IDENT) {
+        init->name.value = token.literal;
+        init->name.line = token.line;
+        init->name.col = token.col;
+    } else {
+        parser_show_expected(parser, IDENT, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    parser_current(parser, &token);
+    if (token.type != COLON) {
+        parser_show_expected(parser, COLON, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    parser_current(parser, &token);
+    if (token.type == CLASS_NAME) {
+        init->type.value = token.literal;
+        init->type.line = token.line;
+        init->type.col = token.col;
+    } else {
+        parser_show_expected(parser, CLASS_NAME, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    parser_current(parser, &token);
+    if (token.type == ASSIGN) {
+        parser_advance(parser);
+
+        build_expr(parser, init->init);
+    }
+}
+
+static void build_expr_let(struct parser *parser, expr_node *expr) {
+    struct token token;
+
+    expr->type = EXPR_LET;
+    ds_dynamic_array_init(&expr->let.inits, sizeof(let_init_node));
+    expr->let.body = malloc(sizeof(expr_node));
+
+    parser_current(parser, &token);
+    if (token.type != LET) {
+        parser_show_expected(parser, LET, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    parser_current(parser, &token);
+    if (token.type == IDENT) {
+        let_init_node init;
+
+        build_let_init(parser, &init);
+
+        ds_dynamic_array_append(&expr->let.inits, &init);
+    } else {
+        parser_show_expected(parser, IDENT, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+
+    parser_current(parser, &token);
+    while (token.type != IN) {
+        if (token.type != COMMA) {
+            parser_show_extected_2(parser, COMMA, IN, token.type);
+            if (token.type == END) {
+                return;
+            }
+        }
+        parser_advance(parser);
+
+        let_init_node init;
+
+        build_let_init(parser, &init);
+
+        ds_dynamic_array_append(&expr->let.inits, &init);
+
+        parser_current(parser, &token);
+    }
+    parser_advance(parser);
+
+    build_expr(parser, expr->let.body);
+}
+
+static void build_branch(struct parser *parser, branch_node *branch) {
+    struct token token;
+
+    branch->name.value = NULL;
+    branch->type.value = NULL;
+    branch->body = malloc(sizeof(expr_node));
+
+    parser_current(parser, &token);
+    if (token.type == IDENT) {
+        branch->name.value = token.literal;
+        branch->name.line = token.line;
+        branch->name.col = token.col;
+    } else {
+        parser_show_expected(parser, IDENT, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    parser_current(parser, &token);
+    if (token.type != COLON) {
+        parser_show_expected(parser, COLON, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    parser_current(parser, &token);
+    if (token.type == CLASS_NAME) {
+        branch->type.value = token.literal;
+        branch->type.line = token.line;
+        branch->type.col = token.col;
+    } else {
+        parser_show_expected(parser, CLASS_NAME, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    parser_current(parser, &token);
+    if (token.type != ARROW) {
+        parser_show_expected(parser, ARROW, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    build_expr(parser, branch->body);
+}
+
+static void build_expr_case(struct parser *parser, expr_node *expr) {
+    struct token token;
+
+    expr->type = EXPR_CASE;
+    expr->case_.expr = malloc(sizeof(expr_node));
+    ds_dynamic_array_init(&expr->case_.cases, sizeof(branch_node));
+
+    parser_current(parser, &token);
+    if (token.type != CASE) {
+        parser_show_expected(parser, CASE, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    build_expr(parser, expr->case_.expr);
+
+    parser_current(parser, &token);
+    if (token.type != OF) {
+        parser_show_expected(parser, OF, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    parser_current(parser, &token);
+    do {
+        branch_node branch;
+
+        build_branch(parser, &branch);
+
+        ds_dynamic_array_append(&expr->case_.cases, &branch);
+
+        parser_current(parser, &token);
+        if (token.type != SEMICOLON) {
+            parser_show_expected(parser, SEMICOLON, token.type);
+            if (token.type == END || token.type == ESAC) {
+                return;
+            }
+        }
+        parser_advance(parser);
+
+        parser_current(parser, &token);
+    } while (token.type != ESAC);
+    parser_advance(parser);
+}
+
+static void build_expr_new(struct parser *parser, expr_node *expr) {
+    struct token token;
+
+    expr->type = EXPR_NEW;
+    expr->new.value = NULL;
+
+    parser_current(parser, &token);
+    if (token.type != NEW) {
+        parser_show_expected(parser, NEW, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    parser_current(parser, &token);
+    if (token.type == CLASS_NAME) {
+        expr->new.value = token.literal;
+        expr->new.line = token.line;
+        expr->new.col = token.col;
+    } else {
+        parser_show_expected(parser, CLASS_NAME, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+}
+
+static void build_expr_isvoid(struct parser *parser, expr_node *expr) {
+    struct token token;
+
+    expr->type = EXPR_ISVOID;
+    expr->isvoid = malloc(sizeof(expr_node));
+
+    parser_current(parser, &token);
+    if (token.type != ISVOID) {
+        parser_show_expected(parser, ISVOID, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    build_expr(parser, expr->isvoid);
+}
+
+static void build_expr_neg(struct parser *parser, expr_node *expr) {
+    struct token token;
+
+    expr->type = EXPR_NEG;
+    expr->neg.expr = malloc(sizeof(expr_node));
+
+    parser_current(parser, &token);
+    if (token.type != TILDE) {
+        parser_show_expected(parser, TILDE, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    build_expr(parser, expr->neg.expr);
+}
+
+static void build_expr_not(struct parser *parser, expr_node *expr) {
+    struct token token;
+
+    expr->type = EXPR_NOT;
+    expr->not.expr = malloc(sizeof(expr_node));
+
+    parser_current(parser, &token);
+    if (token.type != NOT) {
+        parser_show_expected(parser, NOT, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    build_expr(parser, expr->not.expr);
+}
+
+static void build_expr_paren(struct parser *parser, expr_node *expr) {
+    struct token token;
+
+    expr->type = EXPR_PAREN;
+    expr->paren = malloc(sizeof(expr_node));
+
+    parser_current(parser, &token);
+    if (token.type != LPAREN) {
+        parser_show_expected(parser, LPAREN, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+
+    build_expr(parser, expr->paren);
+
+    parser_current(parser, &token);
+    if (token.type != RPAREN) {
+        parser_show_expected(parser, RPAREN, token.type);
+        if (token.type == END) {
+            return;
+        }
+    }
+    parser_advance(parser);
+}
+
 static void build_expr(struct parser *parser, expr_node *expr) {
     struct token token;
 
+    expr->type = EXPR_NONE;
+
     parser_current(parser, &token);
     switch (token.type) {
+    case IF: {
+        build_expr_if(parser, expr);
+        break;
+    }
+    case WHILE: {
+        build_expr_while(parser, expr);
+        break;
+    }
+    case LBRACE: {
+        build_expr_block(parser, expr);
+        break;
+    }
+    case LET: {
+        build_expr_let(parser, expr);
+        break;
+    }
+    case CASE: {
+        build_expr_case(parser, expr);
+        break;
+    }
+    case NEW: {
+        build_expr_new(parser, expr);
+        break;
+    }
+    case ISVOID: {
+        build_expr_isvoid(parser, expr);
+        break;
+    }
+    case TILDE: {
+        build_expr_neg(parser, expr);
+        break;
+    }
+    case NOT: {
+        build_expr_not(parser, expr);
+        break;
+    }
+    case LPAREN: {
+        build_expr_paren(parser, expr);
+        break;
+    }
     case INT_LITERAL: {
         expr->type = EXPR_INT;
         expr->integer.value = token.literal;
@@ -159,6 +617,9 @@ static void build_expr(struct parser *parser, expr_node *expr) {
 
 static void build_attribute(struct parser *parser, attribute_node *attribute) {
     struct token token;
+
+    attribute->name.value = NULL;
+    attribute->type.value = NULL;
 
     parser_current(parser, &token);
     if (token.type == IDENT) {
@@ -206,6 +667,9 @@ static void build_attribute(struct parser *parser, attribute_node *attribute) {
 static void build_formal(struct parser *parser, formal_node *formal) {
     struct token token;
 
+    formal->name.value = NULL;
+    formal->type.value = NULL;
+
     parser_current(parser, &token);
     if (token.type == IDENT) {
         formal->name.value = token.literal;
@@ -245,6 +709,10 @@ static void build_formal(struct parser *parser, formal_node *formal) {
 static void build_method(struct parser *parser, method_node *method) {
     struct token token;
 
+    method->name.value = NULL;
+    method->type.value = NULL;
+    ds_dynamic_array_init(&method->formals, sizeof(formal_node));
+
     parser_current(parser, &token);
     if (token.type == IDENT) {
         method->name.value = token.literal;
@@ -270,7 +738,6 @@ static void build_method(struct parser *parser, method_node *method) {
     parser_current(parser, &token);
     if (token.type == IDENT) {
         struct formal_node formal;
-        formal_node_init(&formal);
 
         build_formal(parser, &formal);
 
@@ -281,7 +748,6 @@ static void build_method(struct parser *parser, method_node *method) {
 
     while (token.type != RPAREN) {
         struct formal_node formal;
-        formal_node_init(&formal);
 
         if (token.type != COMMA) {
             parser_show_extected_2(parser, COMMA, RPAREN, token.type);
@@ -354,14 +820,12 @@ static void build_feature(struct parser *parser, class_node *class) {
     parser_peek(parser, &token);
     if (token.type == COLON) {
         attribute_node attribute;
-        attribute_node_init(&attribute);
 
         build_attribute(parser, &attribute);
 
         ds_dynamic_array_append(&class->attributes, &attribute);
     } else {
         method_node method;
-        method_node_init(&method);
 
         build_method(parser, &method);
 
@@ -371,6 +835,11 @@ static void build_feature(struct parser *parser, class_node *class) {
 
 static void build_class(struct parser *parser, class_node *class) {
     struct token token;
+
+    class->name.value = NULL;
+    class->superclass.value = NULL;
+    ds_dynamic_array_init(&class->attributes, sizeof(attribute_node));
+    ds_dynamic_array_init(&class->methods, sizeof(method_node));
 
     parser_current(parser, &token);
     if (token.type != CLASS) {
@@ -449,7 +918,6 @@ static void build_program(struct parser *parser, program_node *program) {
     struct token token;
     do {
         class_node class;
-        class_node_init(&class);
 
         build_class(parser, &class);
 
