32baa71ffdfb38a2bec2908a1caeffc1a7c34205
Alex Jercan <jercan_alex27@yahoo.com>
2024-03-01 09:27:41 +0200

    feat: added attributes

--- c/src/include/semantic.h
+++ c/src/include/semantic.h
@@ -3,9 +3,15 @@
 
 #include "parser.h"
 
+typedef struct attribute_info {
+        node_info name;
+        node_info type;
+} attribute_info;
+
 typedef struct class_context {
         node_info name;
         struct class_context *parent;
+        ds_dynamic_array attributes;
 } class_context;
 
 typedef struct program_context {
--- c/src/semantic.c
+++ c/src/semantic.c
@@ -24,6 +24,8 @@ static void context_show_errorf(struct program_context *context,
     printf("\n");
 }
 
+// Error messages
+
 #define class_redefined_error(context, class)                                  \
     context_show_errorf(context, class.line, class.col,                        \
                         "Class %s is redefined", class.value)
@@ -44,6 +46,82 @@ static void context_show_errorf(struct program_context *context,
 #define class_inheritance_cycle_error(context, class)                          \
     context_show_errorf(context, class.line, class.col,                        \
                         "Inheritance cycle for class %s", class.value)
+
+#define attribute_illegal_error(context, class, attribute)                     \
+    context_show_errorf(context, attribute.line, attribute.col,                \
+                        "Class %s has attribute with illegal name %s",         \
+                        class.value, attribute.value)
+
+#define attribute_redefined_error(context, class, attribute)                   \
+    context_show_errorf(context, attribute.line, attribute.col,                \
+                        "Class %s redefines attribute %s", class.value,        \
+                        attribute.value)
+
+#define attribute_undefined_type_error(context, class, attr, type)             \
+    context_show_errorf(context, type.line, type.col,                          \
+                        "Class %s has attribute %s with undefined type %s",    \
+                        class.value, attr.value, type.value)
+
+#define attribute_redefined_parent_error(context, class, attribute)            \
+    context_show_errorf(context, attribute.line, attribute.col,                \
+                        "Class %s redefines inherited attribute %s",           \
+                        class.value, attribute.value)
+
+// Error checking
+
+static int is_class_redefined(program_context *context, const char *name) {
+    for (unsigned int i = 0; i < context->classes.count; i++) {
+        struct class_context class;
+        ds_dynamic_array_get(&context->classes, i, &class);
+
+        if (strcmp(class.name.value, name) == 0) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+static int is_class_invalid(const char *name) {
+    if (strcmp(name, "SELF_TYPE") == 0) {
+        return 1;
+    }
+
+    return 0;
+}
+
+static int is_parent_invalid(const char *name) {
+    if (strcmp(name, "Int") == 0 || strcmp(name, "String") == 0 ||
+        strcmp(name, "Bool") == 0 || strcmp(name, "SELF_TYPE") == 0) {
+        return 1;
+    }
+
+    return 0;
+}
+
+static int is_attribute_illegal(const char *name) {
+    if (strcmp(name, "self") == 0) {
+        return 1;
+    }
+
+    return 0;
+}
+
+static int is_attribute_redefined(struct class_context *class,
+                                  const char *name) {
+    for (unsigned int i = 0; i < class->attributes.count; i++) {
+        struct attribute_info attr;
+        ds_dynamic_array_get(&class->attributes, i, &attr);
+
+        if (strcmp(attr.name.value, name) == 0) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+// Implentation
 
 static void context_init(program_context *context) {
     ds_dynamic_array_init(&context->classes, sizeof(struct class_context));
@@ -71,34 +149,17 @@ static void context_init(program_context *context) {
     ds_dynamic_array_append(&context->classes, &io_class);
 }
 
-static int is_class_redefined(program_context *context, const char *name) {
-    for (unsigned int i = 0; i < context->classes.count; i++) {
-        struct class_context class;
-        ds_dynamic_array_get(&context->classes, i, &class);
+static class_context *class_to_context(program_context *context, char *name) {
+    for (unsigned int j = 0; j < context->classes.count; j++) {
+        class_context *c = NULL;
+        ds_dynamic_array_get_ref(&context->classes, j, (void **)&c);
 
-        if (strcmp(class.name.value, name) == 0) {
-            return 1;
+        if (strcmp(c->name.value, name) == 0) {
+            return c;
         }
     }
 
-    return 0;
-}
-
-static int is_class_invalid(program_context *context, const char *name) {
-    if (strcmp(name, "SELF_TYPE") == 0) {
-        return 1;
-    }
-
-    return 0;
-}
-
-static int is_parent_invalid(program_context *context, const char *name) {
-    if (strcmp(name, "Int") == 0 || strcmp(name, "String") == 0 ||
-        strcmp(name, "Bool") == 0 || strcmp(name, "SELF_TYPE") == 0) {
-        return 1;
-    }
-
-    return 0;
+    return NULL;
 }
 
 static void build_class_hierarchy(program_node *program,
@@ -109,10 +170,12 @@ static void build_class_hierarchy(program_node *program,
 
         if (is_class_redefined(context, node.name.value)) {
             class_redefined_error(context, node.name);
-        } else if (is_class_invalid(context, node.name.value)) {
+        } else if (is_class_invalid(node.name.value)) {
             class_invalid_error(context, node.name);
         } else {
             struct class_context class = {.name = node.name, .parent = NULL};
+            ds_dynamic_array_init(&class.attributes,
+                                  sizeof(struct attribute_info));
             ds_dynamic_array_append(&context->classes, &class);
         }
     }
@@ -125,16 +188,7 @@ static void build_parent_hierarchy(program_node *program,
         ds_dynamic_array_get(&program->classes, i, &node);
 
         char *name = node.name.value;
-        class_context *class = NULL;
-        for (unsigned int j = 0; j < context->classes.count; j++) {
-            class_context *c = NULL;
-            ds_dynamic_array_get_ref(&context->classes, j, (void **)&c);
-
-            if (strcmp(c->name.value, name) == 0) {
-                class = c;
-                break;
-            }
-        }
+        class_context *class = class_to_context(context, name);
 
         if (class == NULL) {
             continue;
@@ -145,21 +199,12 @@ static void build_parent_hierarchy(program_node *program,
             parent_name = "Object";
         }
 
-        if (is_parent_invalid(context, parent_name)) {
+        if (is_parent_invalid(parent_name)) {
             class_illegal_parent_error(context, node.name, node.superclass);
             continue;
         }
 
-        class_context *parent_class = NULL;
-        for (unsigned int j = 0; j < context->classes.count; j++) {
-            class_context *c = NULL;
-            ds_dynamic_array_get_ref(&context->classes, j, (void **)&c);
-
-            if (strcmp(c->name.value, parent_name) == 0) {
-                parent_class = c;
-                break;
-            }
-        }
+        class_context *parent_class = class_to_context(context, parent_name);
 
         if (parent_class == NULL) {
             class_undefined_parent_error(context, node.name, node.superclass);
@@ -187,6 +232,84 @@ static void check_inheritance_cycle(program_context *context) {
     }
 }
 
+static void build_class_attributes(program_node *program,
+                                   program_context *context) {
+    for (unsigned int i = 0; i < program->classes.count; i++) {
+        class_node node;
+        ds_dynamic_array_get(&program->classes, i, &node);
+
+        char *name = node.name.value;
+        class_context *class = class_to_context(context, name);
+
+        if (class == NULL) {
+            continue;
+        }
+
+        for (unsigned int j = 0; j < node.attributes.count; j++) {
+            attribute_node attr;
+            ds_dynamic_array_get(&node.attributes, j, &attr);
+
+            if (is_attribute_illegal(attr.name.value)) {
+                attribute_illegal_error(context, node.name, attr.name);
+                continue;
+            }
+
+            if (is_attribute_redefined(class, attr.name.value)) {
+                attribute_redefined_error(context, node.name, attr.name);
+                continue;
+            }
+
+            class_context *type = class_to_context(context, attr.type.value);
+            if (type == NULL) {
+                attribute_undefined_type_error(context, node.name, attr.name,
+                                               attr.type);
+                continue;
+            }
+
+            struct attribute_info info = {.name = attr.name, .type = attr.type};
+            ds_dynamic_array_append(&class->attributes, &info);
+        }
+    }
+}
+
+static void check_parent_class_attributes(program_node *program,
+                                          program_context *context) {
+    for (unsigned int i = 0; i < program->classes.count; i++) {
+        class_node node;
+        ds_dynamic_array_get(&program->classes, i, &node);
+
+        char *name = node.name.value;
+        class_context *class = class_to_context(context, name);
+
+        if (class == NULL) {
+            continue;
+        }
+
+        for (unsigned int j = 0; j < class->attributes.count; j++) {
+            attribute_info attr;
+            ds_dynamic_array_get(&class->attributes, j, &attr);
+
+            class_context *parent = class->parent;
+            int should_run = 1;
+            while (parent != NULL && should_run) {
+                for (unsigned int k = 0; k < parent->attributes.count; k++) {
+                    struct attribute_info parent_attr;
+                    ds_dynamic_array_get(&parent->attributes, k, &parent_attr);
+
+                    if (strcmp(parent_attr.name.value, attr.name.value) == 0) {
+                        attribute_redefined_parent_error(context, class->name,
+                                                         attr.name);
+                        should_run = 0;
+                        break;
+                    }
+                }
+
+                parent = parent->parent;
+            }
+        }
+    }
+}
+
 int semantic_check(program_node *program, program_context *context) {
     context->result = 0;
 
@@ -195,6 +318,9 @@ int semantic_check(program_node *program, program_context *context) {
     build_class_hierarchy(program, context);
     build_parent_hierarchy(program, context);
     check_inheritance_cycle(context);
+
+    build_class_attributes(program, context);
+    check_parent_class_attributes(program, context);
 
     return context->result;
 }
