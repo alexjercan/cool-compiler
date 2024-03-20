74bdc6b53eb28ab02be1e9373aecf9e863997ea2
Alex Jercan <jercan_alex27@yahoo.com>
2024-03-21 00:18:33 +0200

    yikes

--- src/assembler/assembler.c
+++ src/assembler/assembler.c
@@ -630,6 +630,8 @@ static void assembler_emit_tac_jump_if_true(assembler_context *context,
 static void assembler_emit_tac_assign_isinstance(assembler_context *context,
                                                  tac_result tac,
                                                  tac_isinstance instr) {
+    // TODO: does not feel right
+    // we might actually need the dispatch table after all
     const char *comment = NULL;
 
     asm_const *type_const = NULL;
