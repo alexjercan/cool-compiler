3613d40ebb15dad3a60ca574b6cfd2bfbcd9c5ad
Alex Jercan <jercan_alex27@yahoo.com>
2024-03-20 09:34:32 +0200

    docs

--- include/parser.h
+++ include/parser.h
@@ -162,19 +162,19 @@ typedef struct formal_node {
 typedef struct method_node {
         node_info name;
         node_info type;
-        ds_dynamic_array formals;
+        ds_dynamic_array formals; // formal_node
         expr_node body;
 } method_node;
 
 typedef struct class_node {
         node_info name;
         node_info superclass;
-        ds_dynamic_array attributes;
-        ds_dynamic_array methods;
+        ds_dynamic_array attributes; // attribute_node
+        ds_dynamic_array methods; // method_node
 } class_node;
 
 typedef struct program_node {
-        ds_dynamic_array classes;
+        ds_dynamic_array classes; // class_node
 } program_node;
 
 enum parser_result parser_run(const char *filename, ds_dynamic_array *tokens,
